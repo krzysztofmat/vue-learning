5167b9c334c78e86503297f9067010041607dc64
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-07-01 00:22:17 +0200

    lesson9 - parent - child state communication

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,20 +1,17 @@
+import AssignmentCreate from "./AssignmentCreate.js";
 import AssignmentList from "./AssignmentList.js";
 
 export default {
   components: {
     AssignmentList,
+    AssignmentCreate
   },
   template: `
   <section class="space-y-6">
     <assignment-list title="In progress" :assignments="filters.inProgress"></assignment-list>
     <assignment-list title="Completed" :assignments="filters.completed"></assignment-list>
 
-    <form @submit.prevent="add">
-        <div class="border border-gray-600 text-black">    
-            <input v-model="newAssignment" placeholder="New assignment..." class="p-2"/>
-            <button type="submit" class="bg-white p-2 border-l">Add</button>
-        </div>
-    </form>
+    <assignment-create @add="add"/>
   </section>    
     `,
   data() {
@@ -24,7 +21,6 @@ export default {
         { name: "Read chapter 4", complete: false, id: 2 },
         { name: "Turn in homework", complete: false, id: 3 },
       ],
-      newAssignment: "",
     };
   },
   computed: {
@@ -38,14 +34,13 @@ export default {
     },
   },
   methods: {
-    add() {
+    add(newAssignment) {
       const assignment = {
-        name: this.newAssignment,
+        name: newAssignment,
         complete: false,
         id: this.assignments.length + 1,
       };
       this.assignments.push(assignment);
-      this.newAssignment = "";
-    },
+    }
   },
 };
