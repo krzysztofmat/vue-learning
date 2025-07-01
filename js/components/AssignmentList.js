443b6c23fe059f3e057b7c7ae907866d5531e60d
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-07-01 20:05:10 +0200

    add clickable tags to filter assignments

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -6,15 +6,46 @@ export default {
   },
   template: `
     <section v-show="assignments.length">
-      <h2 class="font-bold mb-2">{{ title }}</h2>
-      <ul class="border border-gray-600 divide-y divide-gray-600">
-        <assignment v-for="assignment in assignments" :key="assignment.id" :assignment />
+      <h2 class="font-bold mb-2">
+      {{ title }}
+      <span>({{ assignments.length }})</span> 
+      </h2>
+
+      <div class="flex gap-2">
+        <button 
+          v-for="tag in tags" 
+          class="border rounded px-1 py-px text-xs"
+          :class="{
+            'border-blue-500 text-blue-500': tag === currentTag  
+          }"
+          @click="currentTag = tag"
+          >{{ tag }}
+        </button>
+      </div>
+      
+      <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+        <assignment v-for="assignment in filteredAssignments" :key="assignment.id" :assignment />
       </ul>
     </section>    
     `,
 
+  data() {
+    return {
+      currentTag: 'all'
+    };
+  },
+
   props: {
     assignments: Array,
     title: String,
+  },
+
+  computed: {
+    tags() {
+      return ['all', ...new Set(this.assignments.map((a) => a.tag))];
+    },
+    filteredAssignments() {
+      return this.currentTag !== 'all' ? this.assignments.filter(a => a.tag === this.currentTag) : this.assignments;
+    }
   },
 };
